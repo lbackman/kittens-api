4f3234bda4fea33ab849249948ce1e6a9dc28aa1
lbackman <lucasbackman@gmail.com>
2023-02-08 11:14:54 +0200

    Add flash messages

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
+      flash[:success] = "Great! Your kitten has been created!"
       redirect_to @kitten
     else
+      flash.now[:error] = "Something went wrong! Fix your mistakes, please."
       render :new, status: :unprocessable_entity
     end
   end
@@ -29,8 +31,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash[:success] = "Great! Your kitten has been updated!"
       redirect_to @kitten
     else
+      flash.now[:error] = "Something went wrong! Fix your mistakes, please."
       render :edit, status: :unprocessable_entity
     end
   end
@@ -39,6 +43,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
 
+    flash[:notice] = "You have killed your kitten. You monster!"
     redirect_to root_path, status: :see_other
   end
 
